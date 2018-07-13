7f50076e08b2fd602dc43660c7acdabca49528ce
baasare <asarebernard98@gmail.com>
2018-07-13 16:47:34 +0000

    .

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ def res():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
 #    app.run(debug=True, port=33507)
