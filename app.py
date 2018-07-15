7009512938d331de4040ba99d081ad21315abfc3
baasare <asarebernard98@gmail.com>
2018-07-15 23:27:08 +0000

    expanded currency list

--- app.py
+++ app.py
@@ -2,38 +2,25 @@ import os
 from flask import Flask, render_template, request
 import requests
 import json
-
+import xlrd
 
 app = Flask(__name__)      
 
 
+currencies_d = {}
+wb = xlrd.open_workbook('currency_list.xls')
+sh = wb.sheet_by_index(0)
 
-api_key = "SDX6VUX5ZH5RBERM"
+for i in range(156):
+    code = str(sh.cell(i, 0).value)
+    name = str(sh.cell(i, 1).value)
+    currencies_d[name] = code
 
-#"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=GHS&to_currency=USD&apikey=SDX6VUX5ZH5RBERM"
-
-
-
-
+list_of_currencies = currencies_d.keys()
 
 
 def cedi_to_oth(amount, curr):
-	currency1 = "USD"
-	currency2 = "GBP"
-	currency3 = "EUR"
-
-
-	if curr == currency1:
-		cur = currency1 
-		url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=GHS&to_currency=" + cur + "&apikey=" + api_key
-	elif curr == currency2:
-		cur = currency2 
-		url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=GHS&to_currency=" + cur + "&apikey=" + api_key
-	elif curr == currency3:
-		cur = currency1 
-		url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=GHS&to_currency=" + cur + "&apikey=" + api_key
-	else:
-		print "Sorry please enter a valid option"
+	url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=GHS&to_currency=" + curr + "&apikey=SDX6VUX5ZH5RBERM"
 
 	q = requests.get(url)
 	json_d = q.json()
@@ -47,30 +34,23 @@ def cedi_to_oth(amount, curr):
 	converted_amount = rate * float(amount)
 	converted_amount = round(converted_amount, 2)
 
-
 	return str(converted_amount)
-	
-	 
+
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', list_of_currencies=list_of_currencies)
 
-@app.route('/result', methods = ['POST', 'GET'])
+
+@app.route('/result', methods=['POST', 'GET'])
 def res():
 	if request.method == 'POST':
 		value = request.form['value']
-		currency = request.form['currency']
-		
-		if currency == "USD":
-			denom = " Dollars"
-		elif currency == "GBP":
-			denom = " Pounds"
-		elif currency == "EUR":
-			denom = " Euros"
-		else:
-			denom = "Error"
+		currency_name = request.form['currency']
+		currency = currencies_d[currency_name]
+
 		fresult = cedi_to_oth(value, currency)
-		return render_template('result.html', fresult = fresult, denom = denom)
+		return render_template('result.html', fresult=fresult, currency_name=currency_name)
 
 
 
